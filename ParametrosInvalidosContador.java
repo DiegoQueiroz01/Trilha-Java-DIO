54074f5f0e902b24aa6d8fb167d53b825231f7a8
DiegoQueiroz01 <diqueirozs01@gmail.com>
2024-06-26 18:37:20 -0300

    Add files via upload
    Nesse projeto fiz um contador de números usando o conhecimento de estrutura de controle em Java.

---
+++ ParametrosInvalidosContador.java
@@ -0,0 +1,5 @@
+public class ParametrosInvalidosContador extends Exception {
+    public static void main(String[] args) {
+        System.out.println("O segundo parametro deve ser maior que o primeiro");
+    }
+}
